6a9d85717e376c17ca949d237c798bb46eeaf966
rubensaf <114470625+rubensaf@users.noreply.github.com>
2025-03-30 14:41:18 +0200

    Delete y Fix de la ruta del Get
    Subo directamente los dos , porque lo del GET fue 10s al ver que habia puesto la ruta mal pero llevaba muchas horas y con el cansancio no me di cuenta

--- controllers/endpoint.js
+++ controllers/endpoint.js
@@ -5,7 +5,7 @@ exports.getUserCtrl = async (req, res) => {
         // Buscar el usuario en la base de datos usando el _id que hemos extraído del token JWT
         const user = await UsersModel.findById(req.user._id);
 
-        // Si no encontramos al usuario, devolvemos un error 404
+
         if (!user) {
             return res.status(404).json({ message: 'Usuario no encontrado' });
         }
@@ -20,7 +20,33 @@ exports.getUserCtrl = async (req, res) => {
         });
     } catch (error) {
         console.error(error);
-        // Si ocurre algún error en la consulta o en el servidor, devolvemos un error 500
+ 
         res.status(500).json({ message: 'Error al obtener los datos del usuario' });
+    }
+};
+
+
+exports.deleteUser = async (req, res) => {
+    const { soft } = req.query; // Extraer el parámetro query ?soft=false
+
+    try {
+        const user = await UsersModel.findById(req.user._id);
+        if (!user) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        if (soft === 'false') {
+            // Hard delete (eliminar de la base de datos permanentemente)
+            await user.deleteOne();
+            return res.status(200).json({ message: 'Usuario eliminado permanentemente' });
+        } else {
+            // Soft delete (solo desactivarlo cambiando el estado)
+            user.status = 'inactive';
+            await user.save();
+            return res.status(200).json({ message: 'Usuario desactivado' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al eliminar el usuario' });
     }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const { registerCtrl, loginCtrl , validateEmailCtrl } = require("../controllers/auth"); 
 const { validatorRegister, validatorLogin , validateVerificationCode} = require("../validators/auth"); 
 const {authenticate} = require("../middleware/authMiddleware");
-const { getUserCtrl } = require("../controllers/endpoint");
+const { getUserCtrl , deleteUser } = require("../controllers/endpoint");
 
 // Rutas con validadores
 router.post('/register', validatorRegister, registerCtrl);
 router.post('/login', validatorLogin, loginCtrl);
 router.put('/validation', authenticate, validateVerificationCode, validateEmailCtrl); 
 
+//Los EndPoints
 router.get('/user', authenticate, getUserCtrl);
-
+router.delete('/user', authenticate, deleteUser);
 
 module.exports = router;
